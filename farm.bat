dbdbfab760eac321dd742f0b2e4440951eb2ef3e
Luiz Ferreira <luiz.ferreira094@gmail.com>
2025-03-26 19:00:05 -0300

    fix: new version of farm.py

---
+++ farm.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\Luiz Miguel\Documents\GitHub\coltelino
+python farm.py
+pause
--- farm.py
+++ farm.py
@@ -17,9 +17,10 @@ MOUSE_RIGHTDOWN = 0x08
 MOUSE_RIGHTTUP = 0x10
 VK_ALT = 0x12
 VK_1 = 0x31
+VK_2 = 0X32
 VK_4 = 0x34
 VK_5 = 0X35
-
+VK_9 = 0x39
 SCREEN_LEFT = 0
 SCREEN_TOP = 0
 SCREEN_WIDTH = 0
@@ -40,14 +41,17 @@ sell_list = cv2.imread('sell_list.png')
 # define the image to search for on the screen
 sell_list_empty = cv2.imread('sell_list_empty.png')
 
-# # define the image to search for on the screen
-# obb_img = cv2.imread('obb.png')
+# define the image to search for on the screen
+obb_img = cv2.imread('obb.png')
 
-# # define the image to search for on the screen
-# galho_seco_img = cv2.imread('galho_seco.png')
+# define the image to search for on the screen
+galho_seco_img = cv2.imread('galho_seco.png')
 
 # define the image to search for on the screen
 ygg_img = cv2.imread('ygg.png')
+
+# define the image to search for on the screen
+box_thunder_img = cv2.imread('box_of_thunder.png')
 
 
 storage_close_button_img = cv2.imread('close_storage_button.png')
@@ -82,10 +86,10 @@ def get_coordinates_from_item(item):
 
 def send_storage(x, y):
     ctypes.windll.user32.SetCursorPos(x, y)
-    time.sleep(1)
+    time.sleep(0.2)
     # click ctrl
     ctypes.windll.user32.keybd_event(VK_ALT, 0, 0, 0)
-    time.sleep(0.3)
+    time.sleep(0.1)
     # press right mouse button down
     ctypes.windll.user32.mouse_event(MOUSE_RIGHTDOWN, 0, 0, 0, 0)
     time.sleep(0.1)
@@ -106,7 +110,7 @@ def close_storage():
     storage_button = pyautogui.locateOnScreen(storage_close_button_img, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.8)
     x, y = get_coordinates_from_item(storage_button)
     ctypes.windll.user32.SetCursorPos(x, y)
-    time.sleep(0.3)
+    time.sleep(0.1)
     left_click()
 
 def cursor_center(randomLocation=True):
@@ -135,6 +139,8 @@ def sell():
 def sell_other_items():
     teleport(VK_5)
     time.sleep(1)
+    cursor_center(randomLocation=False)
+    time.sleep(0.2)
     left_click()
     time.sleep(0.3)
     sell_button = pyautogui.locateOnScreen(sell_icon, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.8)
@@ -154,6 +160,18 @@ def sell_other_items():
     cursor_center(randomLocation=False)
     print(f"venda realizada!  Hora: {datetime.now()}")
 
+def send_items_to_storage(items):
+    for item in items:
+        item_pixel = pyautogui.locateOnScreen(item, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.8)
+        # time.sleep(0.3)
+        if item_pixel:
+            item_location_x, item_location_y = get_coordinates_from_item(item_pixel)
+            open_storage()
+            time.sleep(0.1)
+            send_storage(item_location_x, item_location_y)
+            time.sleep(0.1)
+            close_storage()
+            teleport(VK_9)
 
 
 time.sleep(5)
@@ -161,43 +179,30 @@ time.sleep(5)
 
 active_window = gw.getActiveWindow()
 # Verifica se a janela ativa foi encontrada
+
+farmed_items = [galho_seco_img, obb_img]
+
 if active_window:
     # Obtém as coordenadas (x, y) e o tamanho (largura, altura) da janela ativa
     SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT = active_window.left, active_window.top, active_window.width, active_window.height
 
     while True:
-        # search for the popup window on the screenshot
-        weight_heavy = pyautogui.locateOnScreen(weight, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.97)
+        try:
+            # search for the popup window on the screenshot
+            weight_heavy = pyautogui.locateOnScreen(weight, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.97)
 
-        if weight_heavy is not None:
-            # obb_item = pyautogui.locateOnScreen(obb_img, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.5)
-            # gs_item = pyautogui.locateOnScreen(galho_seco_img, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.5)
-            ygg_item = pyautogui.locateOnScreen(ygg_img, region=(SCREEN_LEFT, SCREEN_TOP, SCREEN_WIDTH, SCREEN_HEIGHT), confidence=0.6)
-
-            open_storage()
-            
-            # if obb_item:
-            #     obb_location_x, obb_location_y = get_coordinates_from_item(obb_item)
-            #     send_storage(obb_location_x, obb_location_y)
-            # if gs_item:
-            #     gs_location_x, gs_location_y = get_coordinates_from_item(gs_item)
-            #     send_storage(gs_location_x, gs_location_y)
-            if ygg_item:
-                ygg_location_x, ygg_location_y = get_coordinates_from_item(ygg_item)
-                send_storage(ygg_location_x, ygg_location_y)
-            time.sleep(0.3)
-            close_storage()
-            time.sleep(0.2)
-            cursor_center(randomLocation=False)
-            time.sleep(0.4)
-            sell_other_items()
-        
-        cursor_center()
-        skill_key()
-        left_click()
-        teleport(VK_1)
-        time.sleep(2)
-
+            if weight_heavy is not None:
+                # teleport(VK_2)
+                # time.sleep(0.8)
+                send_items_to_storage(farmed_items)
+                # sell_other_items()
+            cursor_center()
+            skill_key()
+            left_click()
+            teleport(VK_1)
+            time.sleep(1.9)
+        except:
+            print("algum erro ocorreu, reiniciando")
 else:
     print("Nenhuma janela ativa encontrada.")    
 
